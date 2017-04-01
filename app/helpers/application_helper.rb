7a650adbf2baccf861dd3ea4de8d4229b5161d37
Jon <aabdul15@earlham.edu>
2017-04-01 19:31:39 +0000

    Redesign layout and color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
   def activity_resources_exist?(activity)
     activity && activity.trackable && activity.owner
   end
+  
+  def resource_name
+    :user
+  end
+  
+  def resource_class
+    User
+  end
+  
+  def resource
+    @resource ||= User.new                          # Set resource to resource if resource is not nil. Otherwise, set resource to User.new
+  end
+  
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]      # Set devise_mapping to devise_mapping if devise_mapping is not nil. Otherwise, set devise_mapping to devise_mappings[:user]
+  end
+  
 end
